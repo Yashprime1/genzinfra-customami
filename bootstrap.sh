c1ec55cfcce23de1b2de8ebd9edb743960a2117d
Yashdeep Shetty <yashdeep@clevertap.com>
2024-04-21 17:46:52 +0530

    creating mongo ami

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ sudo touch /etc/ecs/ecs.config
 sudo mkdir  /home/bamboo
 sudo chmod -R +777 /etc/ecs
 sudo chmod -R +777 /home/bamboo
-sudo echo "ECS_CLUSTER=Dash-ApplicationEcsCluster" >> /etc/ecs/ecs.config
+sudo echo "ECS_CLUSTER=MongoEcsCluster" >> /etc/ecs/ecs.config
 sudo echo "ECS_AVAILABLE_LOGGING_DRIVERS='[\"awsfirelens\",\"json-file\"]'" >> /etc/ecs/ecs.config
 curl -O https://s3.us-west-2.amazonaws.com/amazon-ecs-agent-us-west-2/amazon-ecs-init-latest.x86_64.rpm
 sudo yum localinstall -y amazon-ecs-init-latest.x86_64.rpm
--- packer.pkr.hcl
+++ packer.pkr.hcl
@@ -8,7 +8,7 @@ packer {
 }
 
 source "amazon-ebs" "ubuntu" {
-  ami_name      = "ami_with_ecs_agent_dash"
+  ami_name      = "ami_with_ecs_agent_mongo"
   instance_type = "t2.micro"
   region        = "ap-south-1"
   source_ami_filter {
