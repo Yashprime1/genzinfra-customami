8a89015a110edb382c1a8cf48cce9994e67a2985
Yashdeep Shetty <yashdeep@clevertap.com>
2024-04-21 21:46:20 +0530

    updating mongo with replicakey

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,11 +2,10 @@
 set -euxo pipefail
 sudo mkdir -p /etc/ecs  
 sudo touch /etc/ecs/ecs.config
-sudo mkdir  /home/bamboo
-sudo chmod -R +777 /etc/ecs
-sudo chmod -R +777 /home/bamboo
-sudo echo "ECS_CLUSTER=MongoEcsCluster" >> /etc/ecs/ecs.config
 sudo mkdir -p /mongo/data
+sudo chmod -R +777 /etc/ecs
+sudo chmod -R +777 /mongo/data
+sudo echo "ECS_CLUSTER=MongoEcsCluster" >> /etc/ecs/ecs.config
 sudo openssl rand -base64 756 >  /mongo/data/replicakey
 sudo chmod 400 /mongo/data/replicakey
 sudo echo "ECS_AVAILABLE_LOGGING_DRIVERS='[\"awsfirelens\",\"json-file\"]'" >> /etc/ecs/ecs.config
