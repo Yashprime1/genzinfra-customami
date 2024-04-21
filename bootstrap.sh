4f5433a2a6f5df52b40a721d0e9fd87bf0221d9d
Yashdeep Shetty <yashdeep@clevertap.com>
2024-04-21 21:10:00 +0530

    adding replicakey file in mongo ami

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,8 @@ sudo chmod -R +777 /etc/ecs
 sudo chmod -R +777 /home/bamboo
 sudo echo "ECS_CLUSTER=MongoEcsCluster" >> /etc/ecs/ecs.config
 sudo mkdir -p /mongo/data
+openssl rand -base64 756 >  /mongo/data/replicakey
+chmod 400 /mongo/data/replicakey
 sudo echo "ECS_AVAILABLE_LOGGING_DRIVERS='[\"awsfirelens\",\"json-file\"]'" >> /etc/ecs/ecs.config
 curl -O https://s3.us-west-2.amazonaws.com/amazon-ecs-agent-us-west-2/amazon-ecs-init-latest.x86_64.rpm
 sudo yum localinstall -y amazon-ecs-init-latest.x86_64.rpm
