2bd785397556dc4bbab7159c448227b3506ce931
Yashprime1 <63970982+Yashprime1@users.noreply.github.com>
2024-05-29 21:10:53 +0530

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -38,15 +38,15 @@ cd /tmp  \
 
 
 # JDK 17 added by Jude.
-curl --silent https://corretto.aws/downloads/resources/17.0.1.12.1/amazon-corretto-17.0.1.12.1-linux-x64.tar.gz | tar -C /opt -xzf - && mv /opt/amazon-corretto-17.0.1.12.1-linux-x64 /opt/amazon-corretto-17-linux-x64
+curl --silent https://corretto.aws/downloads/resources/17.0.1.12.1/amazon-corretto-17.0.1.12.1-linux-x64.tar.gz | sudo tar -C /opt -xzf - && mv /opt/amazon-corretto-17.0.1.12.1-linux-x64 /opt/amazon-corretto-17-linux-x64
 # JDK 18 added by Yashdeep
-curl --silent https://corretto.aws/downloads/resources/18.0.2.9.1/amazon-corretto-18.0.2.9.1-linux-x64.tar.gz |  tar -C /opt -xzf - && mv /opt/amazon-corretto-18.0.2.9.1-linux-x64 /opt/amazon-corretto-18-linux-x64
+curl --silent https://corretto.aws/downloads/resources/18.0.2.9.1/amazon-corretto-18.0.2.9.1-linux-x64.tar.gz | sudo tar -C /opt -xzf - && mv /opt/amazon-corretto-18.0.2.9.1-linux-x64 /opt/amazon-corretto-18-linux-x64
 #installing google chrome headless for test cafe
 curl -s "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" -o google-chrome-stable_current_amd64.deb
 sudo apt install -y ./google-chrome-stable_current_amd64.deb
 
 
-curl -s https://archive.apache.org/dist/ant/binaries/apache-ant-1.9.3-bin.tar.gz | tar -v -xz -C /opt/
+curl -s https://archive.apache.org/dist/ant/binaries/apache-ant-1.9.3-bin.tar.gz | sudo  tar -v -xz -C /opt/
 
 # Download and install jq
 # aws s3 cp s3://system-sharedresources-ssms3bucket-ad5ymdxwx113/bamboo-elastic-agent/jq/jq-1.7.1.tar.gz .
@@ -60,42 +60,43 @@ curl -s https://archive.apache.org/dist/ant/binaries/apache-ant-1.9.3-bin.tar.gz
 # ln -s /usr/local/bin/jq /usr/bin/jq 
 
 # Download and install NodeJS.
-aws s3 cp s3://system-sharedresources-ssms3bucket-ad5ymdxwx113/bamboo-elastic-agent/node-v14-18-2/node-v14.18.2-linux-x64.tar.gz - | tar -v -xz -C /opt/ 
-aws s3 cp s3://system-sharedresources-ssms3bucket-ad5ymdxwx113/bamboo-elastic-agent/node-v18-18-2/node-v18.18.2-linux-x64.tar.gz - | tar -v -xz -C /opt/ 
+aws s3 cp s3://system-sharedresources-ssms3bucket-ad5ymdxwx113/bamboo-elastic-agent/node-v14-18-2/node-v14.18.2-linux-x64.tar.gz - | sudo tar -v -xz -C /opt/ 
+aws s3 cp s3://system-sharedresources-ssms3bucket-ad5ymdxwx113/bamboo-elastic-agent/node-v18-18-2/node-v18.18.2-linux-x64.tar.gz - | sudo tar -v -xz -C /opt/ 
 export PATH=$PATH:/opt/node-v14.18.2-linux-x64/bin
 sudo npm install --global yarn
-echo 'export PATH=/opt/node-v14.18.2-linux-x64/bin:$PATH' >> /etc/profile.d/bamboo.sh
+sudo echo 'export PATH=/opt/node-v14.18.2-linux-x64/bin:$PATH' >> /etc/profile.d/bamboo.sh
 
-curl -fL https://getcli.jfrog.io | sh && mv jfrog /usr/bin/ && chmod +x /usr/bin/jfrog
+curl -fL https://getcli.jfrog.io | sh && sudo mv jfrog /usr/bin/ && sudo chmod +x /usr/bin/jfrog
 
 echo "Set maven, java and ant home directories in PATH"
-sed -i 's/^export JAVA_HOME.*/export JAVA_HOME=\/opt\/amazon-corretto-8.332.08.1-linux-x64/' /etc/profile.d/bamboo.sh
-sed -i 's/^export M2_HOME.*/export M2_HOME=\/opt\/apache-maven-3.9.4/' /etc/profile.d/bamboo.sh
-sed -i 's/^export MAVEN_HOME.*/export MAVEN_HOME=\/opt\/apache-maven-3.9.4/' /etc/profile.d/bamboo.sh
+sudo sed -i 's/^export JAVA_HOME.*/export JAVA_HOME=\/opt\/amazon-corretto-8.332.08.1-linux-x64/' /etc/profile.d/bamboo.sh
+sudo sed -i 's/^export M2_HOME.*/export M2_HOME=\/opt\/apache-maven-3.9.4/' /etc/profile.d/bamboo.sh
+sudo sed -i 's/^export MAVEN_HOME.*/export MAVEN_HOME=\/opt\/apache-maven-3.9.4/' /etc/profile.d/bamboo.sh
 echo "Fix Bamboo elastic agent heap size(XMX)"
-sed -i 's@.*MARKER.*@sed -i "s/-Xmx256m/-Xmx1g/" $startupScript@' /opt/bamboo-elastic-agent/bin/bamboo-elastic-agent
+sudo sed -i 's@.*MARKER.*@sed -i "s/-Xmx256m/-Xmx1g/" $startupScript@' /opt/bamboo-elastic-agent/bin/bamboo-elastic-agent
 
-echo 'export PATH=/opt/amazon-corretto-11.0.19.7.1-linux-x64/bin:/opt/apache-maven-3.9.4/bin:$PATH' >> /etc/profile.d/bamboo.sh
+sudo echo 'export PATH=/opt/amazon-corretto-11.0.19.7.1-linux-x64/bin:/opt/apache-maven-3.9.4/bin:$PATH' >> /etc/profile.d/bamboo.sh
 
 echo "Add private key to bamboo home directory"
-echo "export MAVEN_HOME=/opt/apache-maven-3.9.4" >> /etc/profile.d/bamboo.sh
+sudo echo "export MAVEN_HOME=/opt/apache-maven-3.9.4" >> /etc/profile.d/bamboo.sh
 echo "Setting maven home"
 sudo mkdir -p /home/bamboo/.ssh
-echo "StrictHostKeyChecking no" >> /home/bamboo/.ssh/config
-aws secretsmanager get-secret-value --secret-id BambooElasticInstancePrivat-lnGWOmW8ChWA --region eu-west-1 --output text --query 'SecretString' > /home/bamboo/.ssh/id_rsa
+sudo echo "StrictHostKeyChecking no" >> /home/bamboo/.ssh/config
+sudo aws secretsmanager get-secret-value --secret-id BambooElasticInstancePrivat-lnGWOmW8ChWA --region eu-west-1 --output text --query 'SecretString' > /home/bamboo/.ssh/id_rsa
 sudo chown -R bamboo:bamboo /home/bamboo/.ssh
 sudo chmod 400 /home/bamboo/.ssh/id_rsa
 
-echo "fs.file-max=1000000" >> /etc/sysctl.conf
+sudo echo "fs.file-max=1000000" >> /etc/sysctl.conf
 
-echo "bamboo           soft    nofile          900000" >> /etc/security/limits.conf
-echo "bamboo           hard    nofile          900000" >> /etc/security/limits.conf
+sudo echo "bamboo           soft    nofile          900000" >> /etc/security/limits.conf
+sudo echo "bamboo           hard    nofile          900000" >> /etc/security/limits.conf
 
 instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 
 aws ec2 create-tags --resources $instance_id --region eu-west-1 --tags \
-Key=ct-aws:cloudformation:stack-name,Value=System-Atlassian
+Key=ct-aws:cloudformation:stack-name,Value=System-Atlassian \
+Key=role,Value=bamboo-agent
 
-sudo mkdir -p /home/bamboo/bamboo-agent-home/logs
-sudo chown -R bamboo:users /home/bamboo/bamboo-agent-home
+su -c "mkdir -p /home/bamboo/bamboo-agent-home/logs" bamboo
+su -c "chown -R bamboo:users /home/bamboo/bamboo-agent-home" bamboo
 echo "Completed custom changes"
