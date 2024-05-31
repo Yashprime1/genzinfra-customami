78b084c7f84e02084f084efe4fa303082b504a5f
Yashprime1 <63970982+Yashprime1@users.noreply.github.com>
2024-05-31 20:37:03 +0530

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,2 @@
-wget https://github.com/prometheus/node_exporter/releases/download/v1.8.1/node_exporter-1.8.1.linux-amd64.tar.gz
-tar xvfz node_exporter-1.8.1.linux-amd64.tar.gz
-cd node_exporter-1.8.1.linux-amd64
-./node_exporter --web.listen-address=":8090" > node_exporter.log 2>&1 &
-NODE_EXPORTER_PID=$!
-echo "Node exporter started in the background with PID $NODE_EXPORTER_PID"
-echo "Bootstrapped elastic instance"
+apt-get install -y docker 
+docker run -d -p 9100:9100 --name=node_exporter prom/node-exporter
