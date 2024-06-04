ba20e8d0edafe9ee7b666a15d0c60776f07286a5
Yashprime1 <63970982+Yashprime1@users.noreply.github.com>
2024-06-04 16:38:12 +0530

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,8 +8,8 @@ autoreconf -i
 make
 make install
 ln -s /usr/local/bin/jq /usr/bin/jq 
-wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
-tar xvfz node_exporter-1.3.1.linux-amd64.tar.gz
+wget https://github.com/prometheus/node_exporter/releases/download/v1.8.1/node_exporter-1.8.1.linux-amd64.tar.gz
+tar xvfz node_exporter-1.8.1.linux-amd64.tar.gz
 mv node_exporter-1.8.1.linux-amd64/node_exporter /usr/local/bin/
 rm -rf node_exporter-1.8.1.linux-amd64*
 bash -c 'cat > /etc/systemd/system/node_exporter.service <<EOF
