1873ec00d4c2664424bd15c9a7d49e4f5e9260ae
Akshaya Reddy <57187594+Akshaya222@users.noreply.github.com>
2024-06-05 16:20:57 +0530

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,15 @@ autoreconf -i
 make
 make install
 ln -s /usr/local/bin/jq /usr/bin/jq 
+metadata=$(curl -s http://169.254.169.254/latest/meta-data/)
+echo "metadata- $metadata"
+
+instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+security_group_id=$(curl -s http://169.254.169.254/latest/meta-data/security-groups | cut -d ' ' -f 1)
+key_pair_name=$(curl -s http://169.254.169.254/latest/meta-data/public-keys/0/openssh-key | cut -d ' ' -f 3)
+
+echo "instance id - $instance_id  security group id - $security_group_id key-pair name - $key_pair_name"
+
 wget https://github.com/prometheus/node_exporter/releases/download/v1.8.1/node_exporter-1.8.1.linux-amd64.tar.gz
 tar xvfz node_exporter-1.8.1.linux-amd64.tar.gz
 mv node_exporter-1.8.1.linux-amd64/node_exporter /usr/local/bin/
@@ -26,12 +35,6 @@ sudo systemctl daemon-reload
 sudo systemctl unmask node_exporter
 sudo systemctl enable node_exporter
 sudo systemctl start node_exporter &
-metadata=(curl -s http://169.254.169.254/latest/meta-data/)
-echo "metadata- $metadata"
-instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-security_group_id=$(curl -s http://169.254.169.254/latest/meta-data/security-groups | cut -d ' ' -f 1)
-key_pair_name=$(curl -s http://169.254.169.254/latest/meta-data/public-keys/0/openssh-key | cut -d ' ' -f 3)
-echo "instance id - $instance_id  seuciry group id - $security_group_id key-pair name - $key_pair_name"
 
 
 
