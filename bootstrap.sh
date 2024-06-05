ff515109a41b68350e9d44985428f8f0c9078cb7
Akshaya Reddy <57187594+Akshaya222@users.noreply.github.com>
2024-06-05 18:37:53 +0530

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,6 +18,7 @@ autoreconf -i
 make
 make install
 ln -s /usr/local/bin/jq /usr/bin/jq 
+sleep 300
 wget https://github.com/prometheus/node_exporter/releases/download/v1.8.1/node_exporter-1.8.1.linux-amd64.tar.gz
 tar xvfz node_exporter-1.8.1.linux-amd64.tar.gz
 mv node_exporter-1.8.1.linux-amd64/node_exporter /usr/local/bin/
