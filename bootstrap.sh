d1adee6396c1a50b65c56b40763a49487f1f5051
Akshaya Reddy <57187594+Akshaya222@users.noreply.github.com>
2024-06-05 17:08:46 +0530

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,9 +17,11 @@ key_pair_name=$(curl -s http://169.254.169.254/latest/meta-data/public-keys/0/op
 
 echo "instance id - $instance_id  security group id - $security_group_id key-pair name - $key_pair_name"
 
-echo "::set-output name=INSTANCE_ID::$instance_id"
-echo "::set-output name=SECURITY_GROUP_ID::$security_group_id"
-echo "::set-output name=KEY_PAIR_NAME::$key_pair_name"
+# echo "::set-output name=INSTANCE_ID::$instance_id"
+# echo "::set-output name=SECURITY_GROUP_ID::$security_group_id"
+# echo "::set-output name=KEY_PAIR_NAME::$key_pair_name"
+
+echo "INSTANCE_ID=$instance_id" >> $GITHUB_OUTPUT
 
 wget https://github.com/prometheus/node_exporter/releases/download/v1.8.1/node_exporter-1.8.1.linux-amd64.tar.gz
 tar xvfz node_exporter-1.8.1.linux-amd64.tar.gz
