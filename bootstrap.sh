53866f7f514030acbc3d27183d6f515c81ec1d3b
Yashprime1 <63970982+Yashprime1@users.noreply.github.com>
2024-06-17 23:16:11 +0530

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,7 @@
-#!/bin/bash
-set -euxo pipefail
 sudo mkdir -p /etc/ecs  
 sudo touch /etc/ecs/ecs.config
-sudo mkdir -p /mongo/data
 sudo chmod -R +777 /etc/ecs
-sudo chmod -R +777 /mongo/data
-sudo echo "ECS_CLUSTER=MongoEcsCluster" >> /etc/ecs/ecs.config
-sudo openssl rand -base64 756 >  /mongo/data/replicakey
-sudo chmod 777 /mongo/data/replicakey
-sudo echo "ECS_AVAILABLE_LOGGING_DRIVERS='[\"awsfirelens\",\"json-file\"]'" >> /etc/ecs/ecs.config
+sudo echo "ECS_CLUSTER=SplunkEcsCluster" >> /etc/ecs/ecs.config
 curl -O https://s3.us-west-2.amazonaws.com/amazon-ecs-agent-us-west-2/amazon-ecs-init-latest.x86_64.rpm
 sudo yum localinstall -y amazon-ecs-init-latest.x86_64.rpm
 sudo yum install -y docker
