839a74a85e4748b8659b0c13f7df299aa464b06c
Yashprime1 <63970982+Yashprime1@users.noreply.github.com>
2024-09-06 11:39:07 +0530

    creating ami for mongo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 sudo mkdir -p /etc/ecs  
 sudo touch /etc/ecs/ecs.config
 sudo chmod -R +777 /etc/ecs
-sudo echo "ECS_CLUSTER=SplunkEcsCluster" >> /etc/ecs/ecs.config
+sudo echo "ECS_CLUSTER=ApplicationEcsCluster" >> /etc/ecs/ecs.config
 curl -O https://s3.us-west-2.amazonaws.com/amazon-ecs-agent-us-west-2/amazon-ecs-init-latest.x86_64.rpm
 sudo yum localinstall -y amazon-ecs-init-latest.x86_64.rpm
 sudo yum install -y docker
